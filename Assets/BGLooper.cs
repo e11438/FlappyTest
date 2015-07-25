65b7899e6267c3f89019954dfadadc2efe0302ff
e11438 <isankalpa7@gmail.com>
2015-07-25 11:44:13 -0700

    finished basic movements

---
+++ Assets/BGLooper.cs
@@ -0,0 +1,26 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class BGLooper : MonoBehaviour {
+
+	int numBGPanels=8;
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D collider){
+	
+		Debug.Log ("Triggered:" + collider.name);
+
+		float widthOfBGObj = ((BoxCollider2D)collider).size.x;
+
+
+		string name = ((BoxCollider2D)collider).name;
+		Vector3 pos = collider.transform.position;
+		if (name != "Sky") {
+			pos.x += (widthOfBGObj * numBGPanels-0.4f);
+		} else {
+			pos.x += (widthOfBGObj*2-0.4f);
+		}
+
+		collider.transform.position = pos;
+	}
+}
---
+++ Assets/BirdMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+	Vector3 velocity = Vector3.zero;
+	public Vector3 gravity;
+	public Vector3 flapVelocity;
+	public float maxSpeed=5f;
+	public float forwardSpeed = 1f;
+
+	bool didFlap = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	//Do Grphic & input update here
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+			didFlap=true;
+		}
+
+	}
+	
+	// Do physics engine updates here
+	void FixedUpdate () {
+		velocity.x = forwardSpeed;
+		velocity += gravity* Time.deltaTime;
+
+		if (didFlap) {
+			didFlap=false;
+
+			if(velocity.y<0){
+				velocity.y=0;
+			}
+			velocity+=flapVelocity;
+		}
+		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
+
+		transform.position += velocity * Time.deltaTime;
+
+		float angle = 0;
+		if (velocity.y < 0) {
+			angle = Mathf.Lerp (0, -70, -velocity.y / maxSpeed);
+		}
+		transform.rotation = Quaternion.Euler (0, 0, angle);
+	}
+}
